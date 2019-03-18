553d72d8a56dcb9b3e217ad84a2aaf5536c57c63
Sneha S <ssomwanshi@renderedtext.com>
2019-03-18 12:09:40 +0000

    implement fetch and color

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,15 +1,40 @@
 class Bitmap
+    module COLORS
+        WHITE = 'O'
+        VALID_RANGE = ('A'..'Z')
+    end
+
     def initialize(max_row, max_column)
-        raise ArgumentError.new("wrong row length") if max_row <= 0
-        raise ArgumentError.new("wrong row length") if max_column <= 0
-        @bitmap = Array.new(max_row) { Array.new(max_column) }
-    end  
-    
+        raise ArgumentError.new("wrong row length") if max_row <= 0 || max_row > 250
+        raise ArgumentError.new("wrong row length") if max_column <= 0 || max_column > 250
+        @bitmap = Array.new(max_row) { Array.new(max_column, COLORS::WHITE) }
+    end
+
     def max_row_length
         @bitmap.length
-    end    
+    end
 
     def max_column_length
         @bitmap.first.length
     end
-end    
+
+    def fetch(row, column)
+        @bitmap[row][column]
+    end
+
+    def color(row, column, color_alphabet)
+        unless COLORS::VALID_RANGE.include?(color_alphabet)
+            raise(ArgumentError.new("Wrong color alphabet. should be A...Z"))
+        end
+
+        if row < 0 || row > max_row_length
+            raise(ArgumentError.new("Wrong row co-ordinate"))
+        end
+
+        if column < 0 || column > max_column_length
+            raise(ArgumentError.new("Wrong column co-ordinate"))
+        end
+
+        @bitmap[row][column] = color_alphabet
+    end
+end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -8,13 +8,63 @@ RSpec.describe Bitmap do
             expect(bitmap.max_row_length).to equal(3)
             expect(bitmap.max_column_length).to equal(4)
         end
-        
+
         it "should not accept max row length less than or equal to zero" do
             expect { Bitmap.new(0, 3) }.to raise_error(ArgumentError)
-        end    
+        end
 
         it "should not accept max column length less than or equal to zero" do
             expect { Bitmap.new(3, -1) }.to raise_error(ArgumentError)
-        end    
-    end    
-end    
+        end
+
+        it "should not accept max row length greater than 250" do
+            expect { Bitmap.new(400, 9) }.to raise_error(ArgumentError)
+        end
+
+        it "should not accept max column length greater than 250" do
+            expect { Bitmap.new(3, 1000) }.to raise_error(ArgumentError)
+        end
+    end
+
+    context "clear" do
+        it "should set all pixels to white by default" do
+            max_row = 3
+            max_cloumn = 4
+            bitmap = Bitmap.new(max_row, max_cloumn)
+
+            (0..max_row - 1).each do |row|
+                (0..max_cloumn - 1).each do |column|
+                    expect(bitmap.fetch(row, column)).to eql(Bitmap::COLORS::WHITE)
+                end
+            end
+        end
+    end
+
+    context "color" do
+        before do
+            max_row = 3
+            max_cloumn = 4
+            @bitmap = Bitmap.new(max_row, max_cloumn)
+        end
+
+        it "raise argument error if color alphabet is incorrect" do
+            expect { @bitmap.color(1, 1, "#") }.to raise_error(ArgumentError, "Wrong color alphabet. should be A...Z")
+        end
+
+        it "raise argument error if row co-ordinated are out of range " do
+            expect { @bitmap.color(5, 1, "C") }.to raise_error(ArgumentError, "Wrong row co-ordinate")
+        end
+
+        it "raise argument error if column co-ordinated are out of range " do
+            expect { @bitmap.color(3, 11, "C") }.to raise_error(ArgumentError, "Wrong column co-ordinate")
+        end
+
+        it "should color pixel" do
+            expect(@bitmap.fetch(1, 1)).to eql(Bitmap::COLORS::WHITE)
+
+            @bitmap.color(1,1,'C')
+
+            expect(@bitmap.fetch(1, 1)).to eql('C')
+        end
+    end
+end
